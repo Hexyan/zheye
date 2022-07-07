04727836376ca6104a9d693537316f04abd3e347
Hexyan <hexhedonist@outlook.com>
2022-07-07 18:10:42 +0800

    new

---
+++ zheye-servers/zheye-servers/src/main/java/com/evostar/NettyBooter.java
@@ -0,0 +1,17 @@
+package com.evostar;
+
+import com.evostar.netty.WebSocketChatServer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NettyBooter implements ApplicationListener<ContextRefreshedEvent> {
+    @Autowired
+    private WebSocketChatServer webSocketChatServer;
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        webSocketChatServer.start();
+    }
+}
